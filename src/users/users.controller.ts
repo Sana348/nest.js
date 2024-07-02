e9a25eeeaa618bb90dd23f6443e5142b8753ed08
Sana Parveen <misana348@gmail.com>
2024-07-02 15:35:21 +0530

    Refactor UsersController to use ParseIntPipe for query parameter parsing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query,ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
