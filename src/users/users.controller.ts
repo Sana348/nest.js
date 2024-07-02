16d959a0708ca171f46e6380766891d2c5b4a20f
Sana Parveen <misana348@gmail.com>
2024-07-02 16:50:31 +0530

    Refactor UsersController to use ParseIntPipe for query parameter parsing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query,ParseIntPipe } from '@nestjs/common';
-import { UsersService } from './users.service'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+
 @Controller('users')
 export class UsersController {
 
@@ -13,23 +14,23 @@ export class UsersController {
     }
 
     @Get(':id') // GET /users/:id
-    findOne(@Param('id') id: string) {
-        return this.usersService.findOne(+id)
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id)
     }
 
     @Post() // POST /users 
-    create(@Body() createUserDto: CreateUserDto) {
+    create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto)
     }
 
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
-        return this.usersService.update(+id, updateUserDto)
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
+        return this.usersService.update(id, updateUserDto)
     }
 
     @Delete(':id') // DELETE /users/:id
-    delete(@Param('id') id: string) {
-        return this.usersService.delete(+id)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.delete(id)
     }
 
 }
